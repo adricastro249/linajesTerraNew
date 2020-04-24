b0706e1f82b37ad3f7f28ec8bd69bb53b27a2405
aibbya <aibbyale@gmail.com>
2020-04-23 21:59:41 -0300

    add firebase

--- public/main.js
+++ public/main.js
@@ -1,3 +1,18 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyBI6tMFtyPfAyb1pgcdWIZKFa30SyCxaxw",
+    authDomain: "linajesterraweb.firebaseapp.com",
+    databaseURL: "https://linajesterraweb.firebaseio.com",
+    projectId: "linajesterraweb",
+    storageBucket: "linajesterraweb.appspot.com",
+    messagingSenderId: "291695191486",
+    appId: "1:291695191486:web:415c5195c0885ba0c605ef"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
 // MOSTRAR Y OCULTAR SECCIONES
 
 var home = document.getElementById("home")
@@ -419,3 +434,37 @@ document.getElementById('filtrosGaleria').addEventListener('change', function ()
 
 
 }) //cierra el addEventListener de los checkbox
+
+/* =============== CONTACTO =============== */
+
+const form = document.getElementById('contactForm'); // Obtenemos la referencia al formulario
+
+    if(form){ // Si existe nuestro elemento en memoria este se quedara escuchando al evento submit del formulario
+      form.addEventListener('submit', contactForm); // Al momento de enviar el formulario, ejecuta la función "contactform"
+    }
+
+    function contactForm(event) {
+      event.preventDefault(); // Prevenimos el comportamiento por defecto de un formulario (Enviar por URL los parametros)
+      const nombre = document.getElementById('nombre'); // Obtenemos la referencia a cada uno de nuestros elementos del formulario
+      const email = document.getElementById('email');
+      const tlf = document.getElementById('telfs');
+      const mensaje = document.getElementById("mensaje");
+      const data = {
+        'name': nombre.value,
+        'email': email.value,
+        'tlf': intereses.value,
+        'mensaje': mensaje.value
+      }; // Creamos un objecto con todos los elementos de nuestro formulario.
+      saveContactForm(data); // Enviamos la información obtenida por el usuario a la función que se encargara de guardar la información en Firebase
+      form.reset(); // borramos todos los campos. 
+    }
+
+  function saveContactForm(data) {
+    firebase.database().ref('contact').push(data) // Hacemos referencia al método database de el SDK y hacemos referencia el nombre del objeto que contendrá nuestros registros y empujamos los nuevos envios de datos
+      .then(function(){
+        alert('mensaje guardado'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
+      })
+      .catch(function(){
+        alert('mensaje No guardado'); // En caso de ocurrir un error le mostramos al usuario que ocurrió un error.
+      });
+  };
