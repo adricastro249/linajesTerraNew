9dc7b29401305f1875a6f3f7741c9fb06a23a486
Adriana Castro <adrianacastro@Motonetas-iMac.local>
2020-05-25 15:53:20 -0300

    img carousel

--- public/dataGral.js
+++ public/dataGral.js
@@ -12,8 +12,8 @@ var data222 = {
                     "categoria": "faciales",
                     "descripcion": "Para una piel suave, sana y luminosa",
                     "imgPerfil": "images/productos/serum.png",
-                    "imgGrande": "images/productos/imgVarias/Serum/serum8.jpg",
-                    "imgVarias": ["images/productos/imgVarias/Serum/serum10.jpg", "images/productos/imgVarias/Serum/serum10.jpg"],
+
+                    "imgVarias": ["images/productos/imgVarias/Serum/serum1.png", "images/productos/imgVarias/Serum/serum2.png","images/productos/imgVarias/Serum/serum3.png", "images/productos/imgVarias/Serum/serum4.png","images/productos/imgVarias/Serum/serum5.png"],
                     "ingredientes": ["Aceite de semilla de uva", "Aceite de oliva extra virgen", "Aceite de ajonjolí"],
                     "versiones": [],
                     "contenidoNeto": ["10ml"]
@@ -24,8 +24,7 @@ var data222 = {
                     "categoria": "faciales",
                     "descripcion": "Desintoxica, exfolia y nutre tu piel, otorgando suavidad y frescura. Rinde de 10 a 12 aplicaciones",
                     "imgPerfil": "images/productos/arcillasFaciales.png",
-                    "imgGrande": "images/productos/imgVarias/arcillaFacial/arcillaFacial1.jpg",
-                    "imgVarias": ["images/productos/imgVarias/arcillaFacial/arcillaFacial3.jpg", "images/productos/imgVarias/arcillaFacial/arcillaFacial4.jpg"],
+                    "imgVarias": ["images/productos/imgVarias/arcillaFacial/arcillaFacial1.png", "images/productos/imgVarias/arcillaFacial/arcillaFacial2.png", "images/productos/imgVarias/arcillaFacial/arcillaFacial3.png","images/productos/imgVarias/arcillaFacial/arcillaFacial4.png","images/productos/imgVarias/arcillaFacial/arcillaFacial5.png"],
                     "ingredientes": ["Arcilla blanca", "Cacao", "Carbón activado", "Cúrcuma"],
                     "versiones": ["Arcilla blanca", "Carbón activado", "Curcuma"],
                     "contenidoNeto": ["10 aplicaciones"]
@@ -36,8 +35,7 @@ var data222 = {
                     "categoria": "faciales",
                     "descripcion": "Para una piel limpia y tersa",
                     "imgPerfil": "images/productos/barraLimpiadora.png",
-                    "imgGrande": "images/productos/imgVarias/BarraLimpiadoraDeGlicerina/blg8.jpg",
-                    "imgVarias": ["images/productos/imgVarias/BarraLimpiadoraDeGlicerina/blg7.jpg", "images/productos/imgVarias/BarraLimpiadoraDeGlicerina/blg2.jpg"],
+                    "imgVarias": ["images/productos/imgVarias/BarraLimpiadoraDeGlicerina/blg1.png", "images/productos/imgVarias/BarraLimpiadoraDeGlicerina/blg2.png"],
                     "ingredientes": ["Glicerina vegetal", "Plantas", "Avena", "Cacao", "CArbón activado" ],
                     "versiones": ["Caléndula y manzanilla", "Cacao", "Mandarina", "Rosas y lavanda"],
                     "contenidoNeto": ["70gr aproximadamente"]
@@ -58,8 +56,7 @@ var data222 = {
                     "categoria": "corporales",
                     "descripcion": "Por una protección confiable y amable con nuestro cuerpo y el medio ambiente.",
                     "imgPerfil": "images/productos/desodorantes.png",
-                    "imgGrande": "images/productos/imgVarias/Desodorante/desodorante2.jpg",
-                    "imgVarias": ["images/productos/imgVarias/Desodorante/desodorante2.jpg", "images/productos/imgVarias/Desodorante/desodorante3.jpg"],
+                    "imgVarias": ["images/productos/imgVarias/Desodorante/desodorante1.png", "images/productos/imgVarias/Desodorante/desodorante2.png","images/productos/imgVarias/Desodorante/desodorante3.png","images/productos/imgVarias/Desodorante/desodorante4.png","images/productos/imgVarias/Desodorante/desodorante5.png"],
                     "ingredientes": ["Aceite de coco extra virgen", "Manteca de cacao", "Manteca de karité", "Fécula de Maiz", "Bicarbonato de sodio", "Cera de abejas", "Óxido de zinc", "Vit e", "Aceites esenciales"],
                     "versiones": ["Lavanda", "Lemongras"],
                     "contenidoNeto": ["30gr", "50gr"]
@@ -70,8 +67,7 @@ var data222 = {
                     "categoria": "corporales",
                     "descripcion": "Pomada medicinal para tratar la dermatitis, alergias y sarpullido",
                     "imgPerfil": "images/productos/calma.png",
-                    "imgGrande": "images/productos/imgVarias/Calma/calma2.jpg",
-                    "imgVarias": ["images/productos/imgVarias/Calma/calma3.jpg", "images/productos/imgVarias/Calma/calma1.jpg"],
+                    "imgVarias": ["images/productos/imgVarias/Calma/calma1.png"],
                     "ingredientes": ["Caléndula", "Manzanilla", "Aceite de ajonjolí", "Aceite de oliva", "Manteca de cacao", "Manteca de karité", "Cera de abejas", "Vit e"],
                     "versiones": [],
                     "contenidoNeto": ["30g"],
@@ -83,8 +79,7 @@ var data222 = {
                     "categoria": "corporales",
                     "descripcion": "Combate la irritación, inflamación y enrojecimiento de la piel de tu bebé",
                     "imgPerfil": "images/productos/japipompi.png",
-                    "imgGrande": "images/productos/japipompi.jpeg",
-                    "imgVarias": ["images/productos/japipompi.jpeg", "images/productos/japipompi.jpeg"],
+                    "imgVarias": ["images/productos/imgVarias/Japi/japi1.png"],
                     "ingredientes": ["Caléndula", "Manzanilla", "Aceite de ajonjolí", "Aceite de oliva", "Manteca de cacao", "Manteca de karité", "Cera de abejas", "Óxido de zinc", "Vit e"],
                     "versiones": [],
                     "contenidoNeto": ["30g"]
@@ -95,8 +90,7 @@ var data222 = {
                     "categoria": "corporales",
                     "descripcion": "Nuestras sales tienen un poder inigualable para la depuración energética y eliminación de toxina",
                     "imgPerfil": "images/productos/salesTerapeuticas.png",
-                    "imgGrande": "images/productos/imgVarias/salesTerapeuticas/salesT2.jpg",
-                    "imgVarias": ["images/productos/imgVarias/salesTerapeuticas/salesT6.jpg", "images/productos/imgVarias/salesTerapeuticas/salesT7.jpg"],
+                    "imgVarias": ["images/productos/imgVarias/salesTerapeuticas/salesT1.png", "images/productos/imgVarias/salesTerapeuticas/salesT2.png","images/productos/imgVarias/salesTerapeuticas/salesT3.png","images/productos/imgVarias/salesTerapeuticas/salesT4.png"],
                     "ingredientes": ["Sal marina", "Plantas", "Aceites escenciales"],
                     "versiones": ["Citrus", "Floral"],
                     "contenidoNeto": ["70 gr"]
@@ -108,8 +102,7 @@ var data222 = {
                     "categoria": "corporales",
                     "descripcion": "Humecta y nutre profundamente, derritiéndose sobre tu piel",
                     "imgPerfil": "images/productos/mantequilla.png",
-                    "imgGrande": "images/productos/imgVarias/Mantequilla/mantquilla3.jpg",
-                    "imgVarias": ["images/productos/imgVarias/Mantequilla/mantquilla1.jpg", "images/productos/imgVarias/Mantequilla/mantquilla2.jpg"],
+                    "imgVarias": ["images/productos/imgVarias/Mantequilla/mantquilla1.png", "images/productos/imgVarias/Mantequilla/mantquilla2.png","images/productos/imgVarias/Mantequilla/mantquilla3.png"],
                     "ingredientes": ["Mixtura de aceites vegetales", "Coco", "Semilla aguacate", "Ajonjolí", "Ricino", "Linaza", "Oliva", "Aceites esenciales romero"],
                     "versiones": [],
                     "contenidoNeto": ["220gr"]
@@ -120,8 +113,7 @@ var data222 = {
                     "categoria": "corporales",
                     "descripcion": "Estimula la circulación y elimina con delicadeza la piel muerta de todo tu cuerpo",
                     "imgPerfil": "images/productos/jabonExfoliante.png",
-                    "imgGrande": "images/productos/jabonExfoliante.jpeg",
-                    "imgVarias": ["images/productos/jabonExfoliante.jpeg", "images/productos/jabonExfoliante.jpeg"],
+                    "imgVarias": ["images/productos/imgVarias/JabonExfoliante/jabonExfoliante1.png"],
                     "ingredientes": ["Base de jabón", "Azúcar", "Café", "Cascarilla de cacao", "Aceite de coco virgen", "Plantas", "Vit e"],
                     "versiones": [],
                     "contenidoNeto": ["200gr"]
@@ -132,8 +124,7 @@ var data222 = {
                     "categoria": "corporales",
                     "descripcion": "Humecta, nutre y revitaliza tu piel",
                     "imgPerfil": "images/productos/herbal.png",
-                    "imgGrande": "images/productos/imgVarias/Herbal/herbal4.jpg",
-                    "imgVarias": ["images/productos/imgVarias/Herbal/herbal1.jpg", "images/productos/imgVarias/Herbal/herbal3.jpg"],
+                    "imgVarias": ["images/productos/imgVarias/Herbal/herbal1.png", "images/productos/imgVarias/Herbal/herbal2.png","images/productos/imgVarias/Herbal/herbal3.png"],
                     "ingredientes": ["Aceite de cacao", "Manteca de cacao", "Cera de abejas", "Vit e", "Aceites esenciales"],
                     "versiones": [],
                     "contenidoNeto": ["20gr"]
@@ -144,8 +135,7 @@ var data222 = {
                     "categoria": "corporales",
                     "descripcion": "Humecta, nutre y revitaliza tu piel",
                     "imgPerfil": "images/productos/chocoamor.png",
-                    "imgGrande": "images/productos/imgVarias/Chocoamor/chocoamor2.jpg",
-                    "imgVarias": ["images/productos/imgVarias/Chocoamor/chocoamor3.jpg", "images/productos/imgVarias/Chocoamor/chocoamor4.jpg"],
+                    "imgVarias": ["images/productos/imgVarias/Chocoamor/chocoamor1.png", "images/productos/imgVarias/Chocoamor/chocoamor2.png","images/productos/imgVarias/Chocoamor/chocoamor3.png"],
                     "ingredientes": ["Aceite de cacao", "Manteca de cacao", "Licor de cacao", "Cera e abejas", "Vitamina e"],
                     "versiones": [],
                     "contenidoNeto": ["20gr"]
@@ -156,8 +146,7 @@ var data222 = {
                     "categoria": "corporales",
                     "descripcion": "Humecta, nutre y revitaliza tu piel",
                     "imgPerfil": "images/productos/natural.png",
-                    "imgGrande": "images/productos/imgVarias/Natural/natural3.jpg",
-                    "imgVarias": ["images/productos/imgVarias/Natural/natural4.jpg", "images/productos/imgVarias/Natural/natural5.jpg"],
+                    "imgVarias": ["images/productos/imgVarias/Natural/natural1.png", "images/productos/imgVarias/Natural/natural2.png"],
                     "ingredientes": ["Aceite de coco", "Manteca de cacao", "Cera e abejas", "Vitamina e"],
                     "versiones": [],
                     "contenidoNeto": ["20gr"]
@@ -178,8 +167,7 @@ var data222 = {
                     "categoria": "capilares",
                     "descripcion": "Mixtura de aceites vegetales. Repara, protege, nutre y estimula el crecimiento del cabello",
                     "imgPerfil": "images/productos/bomba.png",
-                    "imgGrande": "images/productos/imgVarias/Bomba/bomba4.jpg",
-                    "imgVarias": ["images/productos/imgVarias/Bomba/bomba2.jpg", "images/productos/imgVarias/Bomba/bomba5.jpg"],
+                    "imgVarias": ["images/productos/imgVarias/Bomba/bomba1.png", "images/productos/imgVarias/Bomba/bomba2.png", "images/productos/imgVarias/Bomba/bomba3.png", "images/productos/imgVarias/Bomba/bomba4.png"],
                     "ingredientes": ["Mixtura de aceites vegetales", "Coco", "Semilla aguacate", "Ajonjolí", "Ricino", "Linaza", "Oliva", "Aceites esenciales romero"],
                     "versiones": [],
                     "contenidoNeto": ["30ml"]
@@ -191,8 +179,7 @@ var data222 = {
                     "categoria": "capilares",
                     "descripcion": "Define tu peinado, a la vez que nutre tu cabello",
                     "imgPerfil": "images/productos/linaza.png",
-                    "imgGrande": "images/productos/linaza.jpeg",
-                    "imgVarias": ["images/productos/linaza.jpeg", "images/productos/linaza.jpeg"],
+                    "imgVarias": ["images/productos/imgVarias/Linaza/linaza1.png"],
                     "ingredientes": ["Infusión de clavos", "Linaza", "Extracto de cola de caballo y jarilla", "Aceite de coco", "Aceite esencial de naranja"],
                     "versiones": [],
                     "contenidoNeto": ["120ml"]
@@ -204,8 +191,7 @@ var data222 = {
                     "categoria": "capilares",
                     "descripcion": "Fortalece y estimula el crecimiento del vello y cabello",
                     "imgPerfil": "images/productos/activador.png",
-                    "imgGrande": "images/productos/imgVarias/Activador/activador1.jpg",
-                    "imgVarias": ["images/productos/imgVarias/Activador/activador1.jpg", "iimages/productos/imgVarias/Activador/activador1.jpg"],
+                    "imgVarias": ["images/productos/imgVarias/Activador/activador1.png"],
                     "ingredientes": ["Aceite de ricino", "Aceite de coco"],
                     "versiones": [],
                     "contenidoNeto": ["10ml"]
@@ -227,8 +213,7 @@ var data222 = {
                     "categoria": "aromaterapia",
                     "descripcion": "Aceites vegetales portadores de propiedades curativas y aromáticas. Conecta con la sutileza de las planta",
                     "imgPerfil": "images/productos/oleosMacerados.png",
-                    "imgGrande": "images/productos/imgVarias/Oleos/oleosMacerados3.jpeg",
-                    "imgVarias": ["images/productos/imgVarias/Oleos/oleosMacerados2.jpeg", "images/productos/imgVarias/Oleos/oleosMacerados7.jpg"],
+                    "imgVarias": ["images/productos/imgVarias/Oleos/oleoMacerados1.png", "images/productos/imgVarias/Oleos/oleoMacerados2.png","images/productos/imgVarias/Oleos/oleoMacerados3.png","images/productos/imgVarias/Oleos/oleoMacerados4.png","images/productos/imgVarias/Oleos/oleoMacerados5.png","images/productos/imgVarias/Oleos/oleoMacerados6.png"],
                     "ingredientes": ["Aceite de coco virgen", "Plantas"],
                     "versiones": ["Eucalipto", "Lavanda", "Romero", "Rosa"],
                     "contenidoNeto": ["10ml"]
@@ -239,8 +224,7 @@ var data222 = {
                     "categoria": "aromaterapia",
                     "descripcion": "Déjate seducir por la magia que creará este óleo sobre tu cuerpo. Elaborado para consentir y cuidar de ti. ",
                     "imgPerfil": "images/productos/oleosMasajes.png",
-                    "imgGrande": "images/productos/imgVarias/OleoMasajes/oleoM1.png",
-                    "imgVarias": ["images/productos/imgVarias/OleoMasajes/oleoM1.png", "images/productos/imgVarias/OleoMasajes/oleoM1.png"],
+                    "imgVarias": ["images/productos/imgVarias/OleoMasajes/oleoM1.png", "images/productos/imgVarias/OleoMasajes/oleoM2.png","images/productos/imgVarias/OleoMasajes/oleoM3.png","images/productos/imgVarias/OleoMasajes/oleoM4.png","images/productos/imgVarias/OleoMasajes/oleoM5.png"],
                     "ingredientes": ["Aceite de coco virgen", "Aceites esenciales", "Plantas"],
                     "versiones": ["Canela", "Herbal", "Naranja"],
                     "contenidoNeto": ["24ml"]
